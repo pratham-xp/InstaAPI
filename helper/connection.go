be354b509c650d38fda7c16fca31cd99b7ff2de0
Pratham Jaiswal <59138245+pratham-xp@users.noreply.github.com>
2021-10-09 19:36:40 +0530

    Update connection.go

--- helper/connection.go
+++ helper/connection.go
@@ -1,1 +1,48 @@
+package helper
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func ConnectDB(n string) *mongo.Collection {
+  
+	clientOptions := options.Client().ApplyURI("mongodb+srv://admin:9120322472wasd@cluster0.n3scs.mongodb.net/test")
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Connected to MongoDB!")
+
+	collection := client.Database("InstaClone").Collection(n)
+
+	return collection
+}
+
+type ErrorResponse struct {
+	StatusCode   int    `json:"status"`
+	ErrorMessage string `json:"message"`
+}
+
+func GetError(err error, w http.ResponseWriter) {
+
+	log.Fatal(err.Error())
+	var response = ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   http.StatusInternalServerError,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
+}
